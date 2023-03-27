ede85305acc643085e01a2ffde19f8d22989629f
Barys Smaliak <smboris@yandex.ru>
2023-03-27 22:26:20 +0300

    Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,36 @@
+﻿Main();
+
+void Main() 
+{
+    bool Work = true;
+    while (Work)
+    {
+        System.Console.WriteLine("Решаем задачу? Введите y/n.");
+        string a = System.Console.ReadLine();
+        switch (a) 
+        {
+            case "y": task1(); break;
+            case "n": Work = false; break;
+        }
+
+    }
+}
+
+
+// новая задача
+int i = GetRandom();
+System.Console.WriteLine(i);
+System.Console.WriteLine(RemoveSecondDigit(i));
+
+int GetRandom()
+{
+    return new Random().Next(100, 1000);
+}
+
+int RemoveSecondDigit(int number)
+{
+    int lastNumber = number % 10;
+    int firstNumber = number / 100;
+    int result = firstNumber*10+lastNumber;
+    return result;
+}
